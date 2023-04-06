09868948222953840395db4bd3cca63d9955fbd2
Duy Nguyen <nguyenvanduy2286@gmail.com>
2023-04-06 23:23:08 +0700

    Start app with 2 enviroments

--- customer/src/config/index.js
+++ customer/src/config/index.js
@@ -1,12 +1,11 @@
 const dotEnv = require("dotenv");
-dotEnv.config();
 
-// if (process.env.NODE_ENV !== "prod") {
-//   const configFile = `./.env.${process.env.NODE_ENV}`;
-//   dotEnv.config({ path: configFile });
-// } else {
-//   dotEnv.config();
-// }
+if (process.env.NODE_ENV !== "prod") {
+  const configFile = `./.env.${process.env.NODE_ENV}`;
+  dotEnv.config({ path: configFile });
+} else {
+  dotEnv.config();
+}
 
 module.exports = {
   PORT: process.env.PORT,
