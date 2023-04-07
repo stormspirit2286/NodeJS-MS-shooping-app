3ed42e6df2944b3113d569f37569368c12a5979c
Duy Nguyen <nguyenvanduy2286@gmail.com>
2023-04-07 10:40:32 +0700

    Add app-event for apis

--- gateway/index.js
+++ gateway/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const proxy = require("express-http-proxy");
+const logger = require("morgan");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(logger("dev"));
 
 app.use("/customer", proxy("http://localhost:8001"));
 app.use("/shopping", proxy("http://localhost:8003"));
